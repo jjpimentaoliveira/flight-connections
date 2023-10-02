e80a4959ceda8a78442b1d00aae5fd17556fe0d4
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-10-02 21:48:04 +0100

    add proj to repo

---
+++ Flight Connections/Flight Connections/Flight_ConnectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flight_ConnectionsApp.swift
+//  Flight Connections
+//
+//  Created by José João Pimenta Oliveira on 02/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Flight_ConnectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Flight Connections/Flight ConnectionsUITests/Flight_ConnectionsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Flight_ConnectionsUITestsLaunchTests.swift
+//  Flight ConnectionsUITests
+//
+//  Created by José João Pimenta Oliveira on 02/10/2023.
+//
+
+import XCTest
+
+final class Flight_ConnectionsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
