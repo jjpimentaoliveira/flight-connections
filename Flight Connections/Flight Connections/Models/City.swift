7d34382171e6d6bf6cce6959fdeef947d3de7b4b
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-10-06 23:34:34 +0100

    Added new struct City
    Should be used to map connections into cities

---
+++ Flight Connections/Flight Connections/Models/City.swift
@@ -0,0 +1,11 @@
+//
+//  City.swift
+//  Flight Connections
+//
+//  Created by José João Pimenta Oliveira on 06/10/2023.
+//
+
+struct City {
+    let name: String
+    var connections: [Connection]
+}
