a8e0ff419df7fdc19d51309eb90eb5d09fd958c6
jjpimentaoliveira <developer.pimenta@gmail.com>
2023-10-07 08:27:44 +0100

    Create uniqueCities method
    This method should be used to create a separate sets of cities (departures vs destinations)

--- Flight Connections/Flight Connections/Models/Connections.swift
+++ Flight Connections/Flight Connections/Models/Connections.swift
@@ -7,4 +7,23 @@
 
 struct Connections: Codable {
     let connections: [Connection]
+
+    func uniqueCities() -> (departures: [String], destinations: [String]) {
+        var uniqueDepartureSet = Set<String>()
+        var uniqueDestinationSet = Set<String>()
+
+        for connection in connections {
+            if let departureCity = connection.from {
+                uniqueDepartureSet.insert(departureCity)
+            }
+            if let destinationCity = connection.to {
+                uniqueDestinationSet.insert(destinationCity)
+            }
+        }
+
+        return (
+            departures: Array(uniqueDepartureSet),
+            destinations: Array(uniqueDestinationSet)
+        )
+    }
 }
